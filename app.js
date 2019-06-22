78ff17406fb17a5ef9a9026cb56df431dc565c05
Anders <kiss7medical@gmail.com>
2019-06-22 11:48:22 +0200

    get initial route functional on localhost

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ var express = require('express');
 
 var app = express();
 
+// ROUTES
+// Home
+app.get('/', function(req, res) {
+	res.send("This is a server response on the homepage");
+});
+
+app.listen(5000, function() {
+	console.log("Listening on localhost:5000");
+});
+
